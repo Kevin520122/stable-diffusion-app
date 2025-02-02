bcbaeee0f93ab0ac23b7c632370770d4a6f66e4a
Kevin520122 <zhaoshilong4@gmail.com>
2025-02-02 15:57:22 -0800

    setProxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@
 
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const proxy = {
-  target: 'https://mko6b9drb2.execute-api.us-east-1.amazonaws.com/',
+  target: 'https://z94hdbw4bg.execute-api.us-west-2.amazonaws.com/',
   changeOrigin: true,
 };
 module.exports = function (app) {
