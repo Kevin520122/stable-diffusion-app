1a4591f9801b8a0845b0074007f7c20600873d92
Kevin520122 <zhaoshilong4@gmail.com>
2025-02-02 16:46:35 -0800

    Update cors

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -6,5 +6,5 @@ const proxy = {
   changeOrigin: true,
 };
 module.exports = function (app) {
-  app.use('/test', createProxyMiddleware(proxy));
+  app.use('/add_cors', createProxyMiddleware(proxy));
 };
